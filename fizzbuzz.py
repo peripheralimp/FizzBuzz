c0e72c59bcd1456558c796be67be03b062824b73
peripheralimp <peripheral.imp@gmail.com>
2022-02-08 17:10:29 -0600

    tests written

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,16 @@
 #the fizzybussy project
 #Gavin Webster 2/5/2022
 
-def fizzbuzz_method(inputList):
+def fizzbuzz_func(inputList):
     outputList = []
     for item in inputList:
-        if item % 3 == 0:
-            if item % 5 == 0:
-                outputList.append("FizzBuzz")
-            else:
-                outputList.append("Fizz")
+        if item % 3 == 0 and item % 5 == 0:
+            outputList.append("FizzBuzz")
+        elif item % 3 == 0:
+            outputList.append("Fizz")
         elif item % 5 == 0:
             outputList.append("Buzz")
         else:
             outputList.append("")
     return outputList
+
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,4 +1,20 @@
-import fizzbuzz as fizz
+import unittest
+from fizzbuzz import fizzbuzz_func
 
-list1 = [0, 3, 4, 5, 6, 7, 8, 9, 15, 16, 20, 30, 31]
-print(fizz.fizzbuzz_method(list1))
+#made these items global so I wouldnt have to declare/define in both test functions
+testList = [-15, -5, -3, -2, -1, 0, 3, 4, 5, 9, 15, 16, 30, 150000]
+ANSWERLIST = ['FizzBuzz', 'Buzz', 'Fizz', '', '', 'FizzBuzz', 'Fizz', '', 'Buzz', 'Fizz', 'FizzBuzz', '', 'FizzBuzz', 'FizzBuzz']
+outputList = fizzbuzz_func(testList)
+#testlist -> outputList, which is compared to answerList in tests below
+
+class TestFizzbuzz(unittest.TestCase):
+    #tests individual elements in outputList
+    def test_elements(self):
+        for i in range(len(outputList)):
+            self.assertEqual(outputList[i], ANSWERLIST[i])
+    #tests the whole outputList. redundant?
+    def test_list(self):
+        self.assertListEqual(outputList, ANSWERLIST)
+
+if __name__ == '__main__':
+    unittest.main()
